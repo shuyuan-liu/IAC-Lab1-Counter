7bffffb409c9fe71200f6fad0306e827a033383e
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-10-22 16:02:59 +0100

    complete task 1 regular tasks
    Changed a few variable names to better represent their function, e.g.
    i→cycle, clk→i, tfp→logger. Changed clk to start low so waveform looks
    better with some "setup time" before clock edges.

---
+++ task1/counter_tb.cpp
@@ -0,0 +1,37 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char* argv[])
+{
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+
+    auto top = new Vcounter;
+    auto logger = new VerilatedVcdC;
+    top->trace(logger, 99);
+    logger->open("counter.vcd");
+
+    top->clk = 0;
+    top->rst = 1;
+    top->en = 0;
+
+    for (int cycle = 0; cycle < 300; cycle++) {
+        top->rst = cycle < 2 || cycle == 15;
+        top->en = cycle > 4 && cycle < 13 || cycle > 16;
+
+        for (int i = 0; i < 2; i++) {
+            logger->dump(2 * cycle + i);
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        if (Verilated::gotFinish()) {
+            return 0;
+        }
+    }
+
+    logger->close();
+    return 0;
+}
+
